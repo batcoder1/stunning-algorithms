25cbdc7d779c3a78ec7cd592d484ba80de6c1786
wooltar <enrique.rubio.developer@gmail.com>
2019-04-02 19:19:20 +0200

    deleting only

--- tests/sin.spec.js
+++ tests/sin.spec.js
@@ -3,16 +3,16 @@ const sin = require('../lib/sin')
  
 
 describe("Integer reversal", ()=> {
-    it.only("sin(23) should return 0.3907312 ", () => {
+    it("sin(23) should return 0.3907312 ", () => {
         assert.equal(sin(23), 0.3907312);
     })
-    it.only("sin(40) should return 0.5 ", () => {
+    it("sin(40) should return 0.5 ", () => {
         assert.equal(sin(30), 0.5);
     })
-    it.only("sin(0) should return 0 ", () => {
+    it("sin(0) should return 0 ", () => {
         assert.equal(sin(0), 0);
     })
-    it.only("sin(90) should return 1 ", () => {
+    it("sin(90) should return 1 ", () => {
         assert.equal(sin(90), 1);
     })
 })
